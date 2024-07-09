53b3e4b436b39660f58dad6ff2803a72844cef64
Harry <helrysonmtg@gmail.com>
2024-07-09 18:36:59 -0300

    funçao delete por id

--- src/main/java/com/rodrigueshelryson/workshopmongo/resources/UserResources.java
+++ src/main/java/com/rodrigueshelryson/workshopmongo/resources/UserResources.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,10 +41,16 @@ public class UserResources {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Void> findById(@RequestBody UserDTO objDto){
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
 		User obj = userServices.fromDTO(objDto);
 		obj = userServices.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable String id){
+		userServices.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
